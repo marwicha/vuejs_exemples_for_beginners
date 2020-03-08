709b547360b27d28ec6cd7eb7cbc52d8a5a35114
marwa <marwa.rekik@esprit.tn>
2020-03-08 11:23:49 +0100

    Game in vue js display attacks text

--- chapter-3-game-app-exemple/app.js
+++ chapter-3-game-app-exemple/app.js
@@ -4,7 +4,8 @@ new Vue ({
 
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
 
     methods: {
@@ -12,10 +13,20 @@ new Vue ({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+
+            this.turns = []
         },
 
         attack() {
-            this.monsterHealth -= this.calculateDamage(3, 10)
+
+            var damage = this.calculateDamage(3, 10)
+            this.monsterHealth -= damage
+
+            this.turns.unshift({
+                isPlayer: true,
+                text: `Player hits Monster for ${damage}`
+            })
+
 
             if(this.checkWin()) {
                 return;
@@ -26,7 +37,13 @@ new Vue ({
 
         specialAttack() {
 
-            this.monsterHealth -= this.calculateDamage(10, 20)
+            var damage = this.calculateDamage(10, 20)
+            this.monsterHealth -= damage
+
+            this.turns.unshift({
+                isPlayer: true,
+                text: `Player hits Monster hard for ${damage}`
+            })
 
             if(this.checkWin()) {
                 return;
@@ -37,22 +54,35 @@ new Vue ({
         },
 
         heal() {
-            if (this.playerHealth <=90) {
+            if (this.playerHealth <= 90) {
                 this.playerHealth += 10
                 this.monsterAttacks()
             } else {
                 this.playerHealth = 100
             }
+
+            this.turns.unshift({
+                isPlayer: true,
+                text: `Player heals for 10`
+            })
+
             this.monsterAttacks()
         },
 
         giveUp() {
+            this.gameIsRunning = false
 
         },
 
         monsterAttacks() {
 
-            this.playerHealth -= this.calculateDamage(3, 12)
+            var damage = this.calculateDamage(3, 12)
+            this.playerHealth -= damage
+
+            this.turns.unshift({
+                isPlayer: false,
+                text: `Monster hits Player for ${damage}`
+            })
 
             this.checkWin();
         },
