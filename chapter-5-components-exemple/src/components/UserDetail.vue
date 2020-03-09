97f3d756b92121c208d78598e026337a02cc7afa
marwa <marwa.rekik@esprit.tn>
2020-03-09 16:32:29 +0100

    passing props from parent to child component

---
+++ chapter-5-components-exemple/src/components/UserDetail.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+
+        <p> Username is : {{ name }} </p>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+
+// child component
+
+props: ['name']
+
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
