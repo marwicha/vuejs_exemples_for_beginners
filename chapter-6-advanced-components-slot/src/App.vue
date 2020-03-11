1e0f323ed4e35adea57a968ebc4d15f5d76c8cee
marwa <marwa.rekik@esprit.tn>
2020-03-11 10:43:20 +0100

    chapter-6 advanced component usage with slot

---
+++ chapter-6-advanced-components-slot/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+               
+               <app-quote> 
+
+                   <h2> {{quoteTitle}} </h2>
+
+                   <p> A wonderful Quote </p>
+
+               </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import Quote from './components/Quote.vue'
+
+    export default {
+
+        components: {
+
+            appQuote: Quote
+        },
+
+        data() {
+            return {
+                quoteTitle: "The Quote"
+            }
+        }
+        
+    }
+</script>
+
+<style>
+
+
+</style>
