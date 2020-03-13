dd4c1397c3cbdacee020fb178a818b885d4226bb
marwa <marwa.rekik@esprit.tn>
2020-03-13 10:16:59 +0100

    switch component

--- chapter-8-forms/src/App.vue
+++ chapter-8-forms/src/App.vue
@@ -112,6 +112,12 @@
                     </select>
                 </div>
             </div>
+
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <app-switch v-model="dataSwitch"> </app-switch>
+                </div>
+            </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
@@ -142,7 +148,7 @@
                         </ul>
                         <p>Gender: {{ gender }} </p>
                         <p>Priority: {{ selectedPriority }}</p>
-                        <p>Switched:</p>
+                        <p>Switched: {{ dataSwitch }}</p>
                     </div>
                 </div>
             </div>
@@ -151,7 +157,15 @@
 </template>
 
 <script>
+
+import Switch from './Switch.vue'
+
     export default {
+
+        components : {
+          appSwitch: Switch
+        },
+
        data() {
 
         return {
@@ -165,7 +179,8 @@
             sendMail: [],
             gender: 'Male',
             priorities: ['High', 'Medium', 'Low'],
-            selectedPriority: 'High'
+            selectedPriority: 'High',
+            dataSwitch: true
       }
 },
     }
