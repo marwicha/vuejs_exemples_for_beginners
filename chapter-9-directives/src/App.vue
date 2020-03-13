9ec80e2c585a119c966c2f0732b7147c49545956
marwa <marwa.rekik@esprit.tn>
2020-03-13 15:15:56 +0100

    building custom directives: values, arguments and modifiers

---
+++ chapter-9-directives/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custom Directives</h1>
+
+                <p v-highlight:background.delayed="'red'"> Color this </p>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
---
+++ chapter-9-directives/src/main.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.directive('highlight', {
+
+  bind(el, binding, vnode) {
+
+    //  ARGUMENTS, VALUES AND MODIFIERS :
+
+    // el.style.backgroundColor = 'green'
+
+   // el.style.backgroundColor = binding.value
+
+   let delayed = 0
+
+   if(binding.modifiers['delayed']) {
+     delayed = 3000
+
+     setTimeout(() => {
+      if (binding.arg) {
+
+        el.style.backgroundColor = binding.value 
+    
+       } else {
+        el.style.color = binding.value 
+       }
+     }, delayed)
+
+   }
+
+  }
+})
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
