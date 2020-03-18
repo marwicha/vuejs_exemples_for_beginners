c01c714bccb4ea6287435978a44eef0e720c7e52
marwa <marwa.rekik@esprit.tn>
2020-03-18 18:29:30 +0100

    http requests with vue-resource(old way)

---
+++ chapter-12-http-vue-ressources/src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import App from './App.vue'
+
+// add plugin with use
+Vue.use(VueResource)
+
+// url http globaly
+Vue.http.options.root = 'https://vue-js-project-course.firebaseio.com/data.json'
+
+// Intercepting requests
+Vue.http.interceptors.push((request, next) => {
+
+  if (request.method == 'POST' ) {
+    request.method = 'PUT'
+  }
+
+  // Intercepting responses
+  next(response => {
+       response.json = () => {
+         return  { messages: response.body}
+       }
+  })
+})
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
